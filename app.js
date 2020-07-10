d6e29d394c614a5a134c89741ae19d7737a2689a
Sungwook Kwon <swooki@gmail.com>
2020-07-10 14:59:41 +0000

    fixes bug in file name - should be case sensitive

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ let app = express();
 app.set('view engine', 'ejs');
 
 // Adds color game direstory
+// app.use(express.static(__dirname + '/colorGame'));
+// console.log('Static Directory: ' + __dirname + '/colorGame');
+
 app.use(express.static(__dirname + '/colorGame'));
-console.log('Static Directory: ' + __dirname + '/colorGame');
+
 
 app.get('/', function (req, res) {
 	res.render('home');
