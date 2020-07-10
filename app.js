bbeb61c5e93cc8cd18bfcc19bdd93a233873cac2
Sungwook Kwon <swooki@gmail.com>
2020-07-10 13:30:23 +0000

    fixes bugs and adds process.env.PORT

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 let express = require('express');
 let app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 // Adds color game direstory
-app.use(express.static(__dirname + "/ColorGame"));
+app.use(express.static(__dirname + '/colorGame'));
+console.log('Static Directory: ' + __dirname + '/colorGame');
 
-app.get("/", function(req, res){
-    res.render("home");
+app.get('/', function (req, res) {
+	res.render('home');
 });
 
-app.get("/about", function (req, res) {
-  res.render("about");
+app.get('/about', function (req, res) {
+	res.render('about');
 });
-app.listen(3000);
+
+app.listen(process.env.PORT, function () {
+	console.log('Server Has Started on port ' + process.env.PORT);
+});
