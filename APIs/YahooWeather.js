851788800eaf6f3129021383f56e219b75136e95
Sungwook Kwon <swooki@gmail.com>
2020-07-28 18:28:03 +0000

    hide client id and secret

--- APIs/YahooWeather.js
+++ APIs/YahooWeather.js
@@ -1,22 +1,4 @@
-// My AppsDublinWeather
-// App ID
-// SssqVC67
-// Client ID (Consumer Key)
-// dj0yJmk9ZXc2d1EwQzZuZkFTJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWQz
-// Client Secret (Consumer Secret)
-// a578df7dde37f107f3b84c1f62986ccbb91bbeb5
-// You can use Client ID and Client Secret to access Yahoo APIs protected by OAuth.
 
-// Application Name
-// DublinWeather
-// Description(Optional)
-// Home Page URL(Optional)
-// Redirect URI(s)(Required)
-// https://tranquil-lake-95794.herokuapp.com/about
-
-// Please specify any additional redirect uris.
-// API Permissions
-// Select private user data APIs that your application needs to access.
 
 
 var OAuth = require('oauth');
@@ -26,8 +8,8 @@ var header = {
 var request = new OAuth.OAuth(
     null,
     null,
-    'dj0yJmk9ZXc2d1EwQzZuZkFTJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWQz',
-    'a578df7dde37f107f3b84c1f62986ccbb91bbeb5',
+    '{Client ID}',
+    '{Client Secret}',
     '1.0',
     null,
     'HMAC-SHA1',
